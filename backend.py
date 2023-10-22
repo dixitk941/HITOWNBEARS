5c47ae7bf8ccbd2936a8d62a26cc33bd3711b406
Karan Dixit <97967796+dixitk941@users.noreply.github.com>
2023-10-22 16:16:44 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,15 +2,15 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# Define a list to store form submissions (for demonstration purposes)
+
 form_submissions = []
 
-# Serve the HTML templates
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Handle the contact form submission
+
 @app.route('/submit_contact', methods=['POST'])
 def submit_contact():
     if request.method == 'POST':
@@ -18,8 +18,7 @@ def submit_contact():
         email = request.form['email']
         message = request.form['message']
 
-        # Here, you can save the form data to a database or do other processing
-        # For demonstration purposes, we'll store it in a list
+
         form_data = {
             'name': name,
             'email': email,
